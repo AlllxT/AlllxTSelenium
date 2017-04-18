3b997cc5aa89495f377a10db9b4f221bd16365f5
AlllxT <dragonic.hd@gmail.com>
2017-04-18 21:52:22 +0300

    start
    start

---
+++ src/test/java/com/alllxt/selenium/tests/OpenGoogleTest.java
@@ -0,0 +1,30 @@
+package com.alllxt.selenium.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Created by atribushny on 18.04.2017.
+ */
+public class OpenGoogleTest {
+
+    private WebDriver driver;
+
+
+    @BeforeClass
+    public void setUp(){
+        driver = new ChromeDriver();
+    }
+    @Test
+    public void openGoogleTest(){
+        driver.get("http://www.google.com");
+    }
+
+    @AfterTest
+    public void tearDrown(){
+        driver.quit();
+    }
+}
